0988aae4f1204fd608069a8069460c145bfab39d
Vadim Axelrod <vadim@axelrod.co>
2020-10-30 18:42:51 +0300

    Release

---
+++ build/precache-manifest.e9839365e31486a66798d85fadcb8cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a997c8bc9579a5a1097d19a89d00361a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ffeef5ccc9b6f013d95",
+    "url": "/static/css/main.41bf74d2.chunk.css"
+  },
+  {
+    "revision": "95d7c1cc2cc36e8b67ae",
+    "url": "/static/js/2.94bdbd6d.chunk.js"
+  },
+  {
+    "revision": "84349322be91852307e40018a2c16b3f",
+    "url": "/static/js/2.94bdbd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ffeef5ccc9b6f013d95",
+    "url": "/static/js/main.87840e97.chunk.js"
+  },
+  {
+    "revision": "1f88faeb50602e122cd8",
+    "url": "/static/js/runtime-main.1ae01bb7.js"
+  }
+]);
